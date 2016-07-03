29687f90d1ede4e8a04e3399f8a446568e77f268
soksy <soksy@yahoo.com>
2016-07-03 10:28:55 +1000

    added questions

---
+++ pypinghot.py
@@ -0,0 +1,9 @@
+#pypinghot.py
+#author Soksy - but i really just googled it!
+
+import subprocess
+import os
+
+net = input("What Network: ")
+firstip = input("Start IP: ")
+lastip = input("Last IP: ")
